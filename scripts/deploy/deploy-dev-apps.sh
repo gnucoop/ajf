4c44015fca18224d577f695cb80dce1d24a0dd9a
trik <marco.marche@gnucoop.com>
2019-08-26 12:32:23 +0200

    build: fix dev apps deploy script

--- scripts/deploy/deploy-dev-apps.sh
+++ scripts/deploy/deploy-dev-apps.sh
@@ -7,8 +7,8 @@ yarn gulp clean
 
 yarn gulp stage-deploy:devapp-mat
 
-scp tar cfj - -C dist/packages/dev-app-mat . | ssh gnucoopdevmat@dev-mat.ajf.rocks:../../web
+tar cfj - -C dist/packages/dev-app-mat . | ssh gnucoopdevmat@dev-mat.ajf.rocks:../../web
 
 yarn gulp stage-deploy:devapp-ion
 
-scp tar cfj - -C dist/packages/dev-app-ion . | ssh gnucoopdevion@dev-ion.ajf.rocks:../../web
+tar cfj - -C dist/packages/dev-app-ion . | ssh gnucoopdevion@dev-ion.ajf.rocks:../../web
