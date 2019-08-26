a7c51630e55d55d1426ce0c5384e3884840925b0
trik <marco.marche@gnucoop.com>
2019-08-26 11:27:06 +0200

    build: clean before buiding dev apps

--- scripts/deploy/deploy-dev-apps.sh
+++ scripts/deploy/deploy-dev-apps.sh
@@ -3,6 +3,8 @@
 # The script should immediately exit if any command in the script fails.
 set -e
 
+yarn gulp clean
+
 yarn gulp stage-deploy:devapp-mat
 
 scp tar cfj - -C dist/packages/dev-app-mat . | ssh gnucoopdevmat@dev-mat.ajf.rocks:../../web
