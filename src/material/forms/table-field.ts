733f24f9c6cd7c52f4924d50858cd03934dde74b
trik <marco.marche@gnucoop.com>
2019-09-02 08:43:43 +0200

    feat(material/forms): go to next cell on tab press in table field

--- src/material/forms/table-field.ts
+++ src/material/forms/table-field.ts
@@ -41,4 +41,19 @@ export class AjfTableFieldComponent extends AjfBaseFieldComponent<AjfTableFieldI
     cdr: ChangeDetectorRef, service: AjfFormRendererService, was: AjfWarningAlertService) {
     super(cdr, service, was);
   }
+
+  goToNextCell(ev: KeyboardEvent, indexColumn: number, indexRow: number): void {
+    const rowLength = this.instance.controlsWithLabels[indexRow].length;
+    const currentCell: any = this.instance.controlsWithLabels[indexRow][indexColumn];
+    let nextCell: any = this.instance.controlsWithLabels[indexRow][indexColumn + 1];
+
+    if (indexColumn + 1 >= rowLength) {
+      nextCell = this.instance.controlsWithLabels[indexRow + 1][1];
+    }
+
+    currentCell.show = false;
+    nextCell.show = true;
+    ev.preventDefault();
+    ev.stopPropagation();
+  }
 }
