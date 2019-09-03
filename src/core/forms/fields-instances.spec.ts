924b5a509315468a15a2bff73b37102e0a27c0f1
Nikolai <nikolai@polikurov.com>
2019-09-03 12:59:58 +0200

    pr corrections on order and field

--- src/core/forms/fields-instances.spec.ts
+++ src/core/forms/fields-instances.spec.ts
@@ -1,6 +1,14 @@
-import {isFieldWithChoicesInstance, createFieldWithChoicesInstance,
-    createNode, AjfNodeType, AjfFieldType,
-    AjfChoice} from './public-api';
+import {
+    AjfChoice,
+    AjfEmptyField,
+    AjfFieldType,
+    AjfNodeInstance,
+    AjfNodeType,
+    createField,
+    createFieldWithChoicesInstance,
+    createNode,
+    isFieldWithChoicesInstance,
+} from './public-api';
 
 describe('isFieldWithChoicesInstance', () => {
     it('should return true if the argument is an instance \
@@ -30,8 +38,8 @@ describe('isFieldWithChoicesInstance', () => {
         expect(isFieldWithChoicesInstance(item)).toBe(true);
     });
 
-    it('should return false if the argument is NOT an instance \
-    of type single or multiple choice field', () => {
+    it('should return false if the argument is not a field instance \
+    or is not field instance of type single or multiple choice field', () => {
         const node = createNode({id: 1, name: 'foo', nodeType: AjfNodeType.AjfField, parent: 0});
         const c1: AjfChoice<number> = {label: 'first', value: 0};
         const item = createFieldWithChoicesInstance({
@@ -55,5 +63,16 @@ describe('isFieldWithChoicesInstance', () => {
             }
         }, {});
         expect(isFieldWithChoicesInstance(item)).toBe(false);
+        expect(isFieldWithChoicesInstance(null as unknown as AjfNodeInstance)).toBe(false);
+
+        const fld = createField({
+            id: 999,
+            name: '',
+            parent: -1,
+            fieldType: AjfFieldType.Empty,
+            label: ''
+          }) as AjfEmptyField;
+
+        expect(isFieldWithChoicesInstance(fld as unknown as AjfNodeInstance)).toBe(false);
     });
 });
