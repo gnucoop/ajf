c4c6f3afa5e248aebacbd990e18a0e350be7f87f
trik <marco.marche@gnucoop.com>
2019-09-05 18:18:57 +0200

    docs: core forms unit tests

--- src/core/forms/forms.spec.ts
+++ src/core/forms/forms.spec.ts
@@ -1,10 +1,74 @@
-import {createForm} from './public-api';
+import {TestBed} from '@angular/core/testing';
 
-describe('createForm', () => {
-  it('should create a valid form', () => {
-    let form = createForm();
+import {AjfFieldInstance, AjfForm, AjfFormRendererService, AjfFormSerializer,
+  AjfFormsModule} from '@ajf/core/forms';
 
-    expect(form.choicesOrigins).toEqual([]);
-    expect(form.nodes).toEqual([]);
+describe('formula field', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AjfFormsModule],
+    }).compileComponents();
   });
-});
+
+  it('the sum of two formulae should produce a number \
+  and not a string concatenation', done => {
+    const formRenderer: AjfFormRendererService = TestBed.get(AjfFormRendererService);
+      const json = {
+        nodes: [
+          {
+            id: 1,
+            parent: 0,
+            parentNode: 0,
+            name: 'form',
+            label: 'Form',
+            nodeType: 3,
+            conditionalBranches: [
+              { condition: 'true' }
+            ],
+            nodes: [
+              {
+                parent: 1,
+                parentNode: 0,
+                id: 1001,
+                name: 'formula0',
+                label: 'Formula0',
+                nodeType: 0,
+                fieldType: 6,
+                formula: {
+                  formula: '1'
+                },
+                conditionalBranches: [
+                  { condition: 'true' }
+                ],
+              },
+              {
+                parent: 1001,
+                parentNode: 0,
+                id: 1002,
+                name: 'formula1',
+                label: 'Formula1',
+                nodeType: 0,
+                fieldType: 6,
+                formula: {
+                  formula: 'formula0 + 1'
+                },
+                conditionalBranches: [
+                  { condition: 'true' }
+                ],
+              }
+            ]
+          }
+        ]
+      };
+      const form: AjfForm = AjfFormSerializer.fromJson(json);
+
+      formRenderer.nodesTree.subscribe(nodes => {
+        const slide = nodes[0];
+        const field = slide.nodes[1] as AjfFieldInstance;
+        expect(field.value).toEqual(2);
+        done();
+      });
+
+      formRenderer.setForm(form);
+    });
+  });
