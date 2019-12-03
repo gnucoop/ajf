5cdaa8c69a91efcb5b3b36ae824ca8c68e3703bb
trik <marco.marche@gnucoop.com>
2019-12-03 18:10:41 +0100

    fix(ionic/page-slider): workaround for layout bug in ionic radio ripple

--- src/ionic/page-slider/page-slider.ts
+++ src/ionic/page-slider/page-slider.ts
@@ -20,13 +20,14 @@
  *
  */
 
+import {AjfPageSlider as AjfCorePageSlider, AjfPageSliderItem} from '@ajf/core/page-slider';
 import {AnimationBuilder} from '@angular/animations';
 import {
-  ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChildren,
-  Renderer2, ViewChild, ViewEncapsulation
+  AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component,
+  ContentChildren, ElementRef, OnDestroy, Renderer2, ViewChild, ViewEncapsulation
 } from '@angular/core';
-
-import {AjfPageSlider as AjfCorePageSlider, AjfPageSliderItem} from '@ajf/core/page-slider';
+import {merge, Subscription} from 'rxjs';
+import {filter, map, startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   moduleId: module.id,
@@ -42,12 +43,47 @@ import {AjfPageSlider as AjfCorePageSlider, AjfPageSliderItem} from '@ajf/core/p
     body: new ViewChild('body', {static: true}),
   },
 })
-export class AjfPageSlider extends AjfCorePageSlider {
+export class AjfPageSlider extends AjfCorePageSlider implements AfterContentInit, OnDestroy {
+  private _scrollSub = Subscription.EMPTY;
+
   constructor(
     animationBuilder: AnimationBuilder,
     cdr: ChangeDetectorRef,
     renderer: Renderer2,
+    private _el: ElementRef,
   ) {
     super(animationBuilder, cdr, renderer);
+  }
+
+  ngAfterContentInit(): void {
+    super.ngAfterContentInit();
+    this._scrollSub = this.pages.changes.pipe(
+      map(() => this.pages.toArray()),
+      startWith(this.pages.toArray()),
+      filter(pages => pages.length > 0),
+      switchMap(pages => merge(...pages.map(page => page.scroll))),
+    ).subscribe(() => {
+      try {
+        const el = this._el.nativeElement as HTMLElement;
+        const radioGroups = el.getElementsByTagName('ion-radio-group');
+        const radioGroup = radioGroups[0];
+        const items = radioGroup.getElementsByTagName('ion-item');
+        const item = items[0];
+        const ripples = item.shadowRoot!.firstElementChild!
+          .getElementsByTagName('ion-ripple-effect');
+        const ripple = ripples[0];
+        const orig = ripple.style.opacity;
+        ripple.style.opacity = '0';
+        ripple.addRipple(0, 0).then(remove => {
+          remove();
+          ripple.style.opacity = orig;
+        });
+      } catch (err) { }
+    });
+  }
+
+  ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this._scrollSub.unsubscribe();
   }
 }
