13e1448757965e7339438f280e4ec39ad68fc30a
trik <marco.marche@gnucoop.com>
2020-08-10 17:05:18 +0200

    build: snapshot builds incorrectly modify semver versions
    An interesting case that came up in v10.0.0 with the docs-content. The
    snapshot release package and docs-content output had various
    `@breaking-change` notes in the source code referring to `v10.0.0` as
    certain changes are planned to be made at that point.
    
    The snapshot deploy scripts pick up the version from the package.json
    file and replace it in the output packages with a more concrete version
    that includes the SHA. This meant that we accidentally also overwrote
    versions as in the `@breaking-change` notes (ultimately making it
    difficult for us to use the latest docs-content for the v10 release as
    it was incorrect).
    
    Example snapshot commit:
    https://github.com/angular/material2-docs-content/commit/e624c7130d90026574b46d12d488e470f0a18c55
    
    We fix this by not including the SHA as part of the deployment, but
    rather including the SHA when building the NPM packages. At that point,
    we can safely just replace instances of the `0.0.0-PLACEHOLDER` without
    having to worry about accidental version overriding.
    
    To achieve this, we update our release stamping script to have two
    modes. i.e. snapshot build mode and release mode. Framework does this
    by checking the Git tag history but that seems less ideal as it makes
    the release output building reliant on external factors while our
    stamping is self-contained within the checked out code revision.

--- scripts/local-pipeline.sh
+++ scripts/local-pipeline.sh
@@ -38,7 +38,9 @@ echo "E2E tests"
 
 echo "Release output"
 yarn build
-yarn check-release-output
+pkg_json_version=$(node -pe "require('./package.json').version")
+expected_version="${pkg_json_version}-sha-$(git rev-parse --short HEAD)"
+yarn check-release-output ${expected_version}
 cp -R dist/releases/* node_modules/@ajf/
 mv node_modules/__ngcc_entry_points__.json node_modules/__ngcc_entry_points__.json.back
 yarn ngcc --error-on-failed-entry-point --no-tsconfig
