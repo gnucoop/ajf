f0ab3e78bedbe5857fa2b1753e61f3ddd238115c
trik <marco.marche@gnucoop.com>
2020-11-09 09:17:38 +0100

    build: fix release allowed branches names

--- tools/release/version-name/publish-branches.ts
+++ tools/release/version-name/publish-branches.ts
@@ -12,10 +12,10 @@ export function getAllowedPublishBranches(version: Version): string[] {
     // It's also possible that the caretaker wants to stage a minor release from a different
     // branch than "master". This can happen if major changes have been merged into "master"
     // and non-major changes are cherry-picked into a separate branch (e.g. 7.x)
-    return ['master', `${version.major}.x`];
+    return ['master', `v${version.major}.x`];
   }
 
-  return [`${version.major}.${version.minor}.x`];
+  return [`v${version.major}.${version.minor}.x`];
 }
 
 /** Determines the type of the specified Semver version. */
