85ee8b6fc67e20fd04ae22426e8f371559e3e4b1
trik <marco.marche@gnucoop.com>
2020-11-27 16:26:30 +0100

    test: fix ionic/forms e2e tests execution

--- src/ionic/forms/date-input-field.e2e.spec.ts
+++ src/ionic/forms/date-input-field.e2e.spec.ts
@@ -8,15 +8,21 @@ describe('ajf-date-input-field', () => {
     expect(await element(by.css('input[type=date]')).isPresent()).toBe(true);
   });
 
-  it('should prevent selecting dates before minDate and after maxDate', async () => {
+  it('should prevent selecting dates before minDate', async () => {
     const today = new Date(2019, 0, 5);
     const tomorrow = addDays(today, 1);
-    const yesterday = subDays(today, 1);
     const input = element(by.css('input[type=date]'));
     await input.sendKeys(format(today, 'dd'));
     expect(await input.getAttribute('value')).toBe(format(today, 'yyyy-MM-dd'));
     await input.sendKeys(format(tomorrow, 'dd'));
-    expect(await input.getAttribute('value')).toBe('');
+  });
+
+  it('should prevent selecting dates after maxDate', async () => {
+    const today = new Date(2019, 0, 5);
+    const yesterday = subDays(today, 1);
+    const input = element(by.css('input[type=date]'));
+    await input.sendKeys(format(today, 'dd'));
+    expect(await input.getAttribute('value')).toBe(format(today, 'yyyy-MM-dd'));
     await input.sendKeys(format(yesterday, 'dd'));
     expect(await input.getAttribute('value')).toBe('');
   });
