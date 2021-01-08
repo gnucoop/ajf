ec78707c49559a476b799a3770db73bd90ea2b16
peppedeka <peppedeka@gmail.com>
2021-01-08 13:48:30 +0100

    fix(material/form): input field
    sum between number input field with some null generate concatenation

--- src/core/forms/forms.spec.ts
+++ src/core/forms/forms.spec.ts
@@ -67,42 +67,99 @@ describe('formula field', () => {
        formRenderer.setForm(form);
      });
 
-     it('check field default value',
-        done => {
-          const formRenderer: AjfFormRendererService = TestBed.get(AjfFormRendererService);
-          const json = {
-            nodes: [{
-              id: 1,
-              parent: 0,
-              parentNode: 0,
-              name: 'form',
-              label: 'Form',
-              nodeType: 3,
-              conditionalBranches: [{condition: 'true'}],
-              nodes: [
-                {
-                  parent: 1,
-                  parentNode: 0,
-                  id: 1001,
-                  name: 'formula0',
-                  label: 'Formula0',
-                  nodeType: 0,
-                  fieldType: 2,
-                  defaultValue: 14,
-                  conditionalBranches: [{condition: 'true'}],
-                }
-              ]
-            }]
-          };
-          const form: AjfForm = AjfFormSerializer.fromJson(json);
+  it('the sum of two number input should produce a number \
+     and not a string concatenation when some number input are null',
+     done => {
+       const formRenderer: AjfFormRendererService = TestBed.get(AjfFormRendererService);
+       const json = {
+         nodes: [{
+           id: 1,
+           parent: 0,
+           parentNode: 0,
+           name: 'form',
+           label: 'Form',
+           nodeType: 3,
+           conditionalBranches: [{condition: 'true'}],
+           nodes: [
+             {
+               parent: 1,
+               parentNode: 0,
+               id: 1001,
+               name: 'number1',
+               label: 'Number1',
+               nodeType: 0,
+               fieldType: 2,
+               conditionalBranches: [{condition: 'true'}],
+             },
+             {
+               parent: 1001,
+               parentNode: 0,
+               id: 1002,
+               name: 'number2',
+               label: 'Number2',
+               nodeType: 0,
+               fieldType: 2,
+               conditionalBranches: [{condition: 'true'}],
+             },
+             {
+               parent: 1002,
+               parentNode: 0,
+               id: 1003,
+               name: 'formula',
+               label: 'Formula',
+               nodeType: 0,
+               fieldType: 2,
+               formula: {formula: 'number1 + number2'},
+               conditionalBranches: [{condition: 'true'}],
+             }
+           ]
+         }]
+       };
+       const form: AjfForm = AjfFormSerializer.fromJson(json, {number1: 1});
 
-          formRenderer.nodesTree.subscribe(nodes => {
-            const slide = nodes[0];
-            const field = slide.nodes[0] as AjfFieldInstance;
-            expect(field.value).toEqual(14);
-            done();
-          });
+       formRenderer.nodesTree.subscribe(nodes => {
+         const slide = nodes[0];
+         const field = slide.nodes[2] as AjfFieldInstance;
+         expect(field.value).toEqual(1);
+         done();
+       });
 
-          formRenderer.setForm(form);
-        });
+       formRenderer.setForm(form);
+     });
+
+  it('check field default value', done => {
+    const formRenderer: AjfFormRendererService = TestBed.get(AjfFormRendererService);
+    const json = {
+      nodes: [{
+        id: 1,
+        parent: 0,
+        parentNode: 0,
+        name: 'form',
+        label: 'Form',
+        nodeType: 3,
+        conditionalBranches: [{condition: 'true'}],
+        nodes: [{
+          parent: 1,
+          parentNode: 0,
+          id: 1001,
+          name: 'formula0',
+          label: 'Formula0',
+          nodeType: 0,
+          fieldType: 2,
+          defaultValue: 14,
+          conditionalBranches: [{condition: 'true'}],
+        }]
+      }]
+    };
+    const form: AjfForm = AjfFormSerializer.fromJson(json);
+
+    formRenderer.nodesTree.subscribe(nodes => {
+      const slide = nodes[0];
+      const field = slide.nodes[0] as AjfFieldInstance;
+      expect(field.value).toEqual(14);
+      done();
+    });
+
+    formRenderer.setForm(form);
+  });
 });
