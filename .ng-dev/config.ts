99dac010273edae13426cb28ff319b52db4e82bf
Marco Marche <marco.marche@gnucoop.com>
2021-03-11 15:27:33 +0100

    build: configure the ng-dev caretaker check command
    Create the configuration for the caretaker check command in the repo.

--- .ng-dev/config.ts
+++ .ng-dev/config.ts
@@ -2,10 +2,13 @@ import {commitMessage} from './commit-message';
 import {format} from './format';
 import {github} from './github';
 import {merge} from './merge';
+import {commitMessage} from './commit-message';
+import {caretaker} from './caretaker';
 
 module.exports = {
   commitMessage,
   format,
   github,
   merge,
+  caretaker,
 };
