274b65a5a5d975550d390161ca9b687dd67649f4
Marco Marche <marco.marche@gnucoop.com>
2021-03-11 13:10:56 +0100

    build: bump gic/angular to version v5.6.0
    test: since input value change is not instant when interacting with ionic inputs, we need to wait a few milliseconds before checking the new input value

--- src/ionic/forms/date-input-field.e2e.spec.ts
+++ src/ionic/forms/date-input-field.e2e.spec.ts
@@ -2,7 +2,13 @@ import {addDays, format, subDays} from 'date-fns';
 import {browser, by, element} from 'protractor';
 
 describe('ajf-date-input-field', () => {
-  beforeEach(async () => await browser.get('/ion-date-input-field'));
+  beforeEach(async () => {
+    await browser.get('/ion-date-input-field');
+    await browser.wait(async () => {
+      const classes = await element(by.css('html')).getAttribute('class');
+      return classes.indexOf('hydrated') > -1;
+    }, 5000);
+  });
 
   it('should show a date input field', async () => {
     expect(await element(by.css('input[type=date]')).isPresent()).toBe(true);
@@ -13,8 +19,11 @@ describe('ajf-date-input-field', () => {
     const tomorrow = addDays(today, 1);
     const input = element(by.css('input[type=date]'));
     await input.sendKeys(format(today, 'dd'));
+    await browser.sleep(200);
     expect(await input.getAttribute('value')).toBe(format(today, 'yyyy-MM-dd'));
     await input.sendKeys(format(tomorrow, 'dd'));
+    await browser.sleep(200);
+    expect(await input.getAttribute('value')).toBe('');
   });
 
   it('should prevent selecting dates after maxDate', async () => {
@@ -22,8 +31,10 @@ describe('ajf-date-input-field', () => {
     const yesterday = subDays(today, 1);
     const input = element(by.css('input[type=date]'));
     await input.sendKeys(format(today, 'dd'));
+    await browser.sleep(200);
     expect(await input.getAttribute('value')).toBe(format(today, 'yyyy-MM-dd'));
     await input.sendKeys(format(yesterday, 'dd'));
+    await browser.sleep(200);
     expect(await input.getAttribute('value')).toBe('');
   });
 });
