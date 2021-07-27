1b18d9f936abe136616f06398826b482e99d514e
peppedeka <peppedeka@gmail.com>
2021-07-27 15:37:21 +0200

    fix(core/transloco): add module forRoot function

--- tools/public_api_guard/core/transloco.d.ts
+++ tools/public_api_guard/core/transloco.d.ts
@@ -3,6 +3,7 @@ export declare class AjfTranslocoModule {
     static ɵfac: i0.ɵɵFactoryDeclaration<AjfTranslocoModule, never>;
     static ɵinj: i0.ɵɵInjectorDeclaration<AjfTranslocoModule>;
     static ɵmod: i0.ɵɵNgModuleDeclaration<AjfTranslocoModule, never, [typeof i1.TranslocoModule], [typeof i1.TranslocoModule]>;
+    static forRoot(): ModuleWithProviders<AjfTranslocoModule>;
 }
 
 export declare const langs: {
