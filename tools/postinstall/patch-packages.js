af4671b880fe6b847b8ef3339b9db81afbd7d2b7
trik <marco.marche@gnucoop.com>
2021-10-21 16:13:28 +0200

    build: clean some node_modules dirs on postinstall to speed up build

--- tools/postinstall/patch-packages.js
+++ tools/postinstall/patch-packages.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const ngDevFile = 'node_modules/@angular/dev-infra-private/ng-dev/cli-bundle.js';
 let ngDev = fs.readFileSync(ngDevFile, 'utf8');
@@ -16,3 +17,17 @@ const ioniconsFile = 'node_modules/ionicons/dist/types/components.d.ts';
 let ionicons = fs.readFileSync(ioniconsFile, 'utf8');
 ionicons = ionicons.replace(/"aria(Hidden|Label)"\?:/gm, '"aria$1":');
 fs.writeFileSync(ioniconsFile, ionicons);
+
+const cleanDirs = [
+  '@angular/common/locales',
+  '@angular/core/schematics',
+  '@angular/cdk/schematics',
+  '@angular/material/schematics',
+  '@ngneat/transloco/schematics',
+];
+cleanDirs.forEach(cleanDir => {
+  const cleanDirPath = path.join('node_modules', cleanDir);
+  if (fs.existsSync(cleanDirPath) && fs.statSync(cleanDirPath).isDirectory()) {
+    fs.rmdirSync(cleanDirPath, {recursive: true});
+  }
+});
