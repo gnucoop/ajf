38bac96e398d61316f7ff34d72445b7e1be14fdd
trik <marco.marche@gnucoop.com>
2022-03-18 18:31:59 +0100

    build: restore artifacts publication on successful build

---
+++ scripts/jenkins/create-build-artifacts.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import {default as childProcess} from 'child_process';
+import {default as fs} from 'fs';
+import {default as path} from 'path';
+
+import {packages} from '../build-config.mjs';
+
+const cwd = process.cwd();
+const mainPackage = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf8'));
+const baseVersion = mainPackage.version;
+const sha = childProcess.execSync('git rev-parse --short HEAD').toString('utf-8').trim();
+const version = `${baseVersion}-${sha}`;
+
+for (const pkg of packages) {
+  const packageDir = path.join(cwd, 'dist', pkg);
+  const packageJsonPath = path.join(packageDir, 'package.json');
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+  packageJson.version = version;
+  for (const dep in packageJson.dependencies) {
+    if (dep.startsWith('@ajf') && packageJson.dependencies[dep] === baseVersion) {
+      packageJson.dependencies[dep] = version;
+    }
+  }
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  childProcess.execSync(`cd ${packageDir} ; yarn pack`);
+}
