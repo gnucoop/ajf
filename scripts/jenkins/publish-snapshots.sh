ad1f946a69789f477d4cbca886c209fd246683b5
Marco Tozzi <marco.tozzi@gnucoop.com>
2023-02-27 16:37:37 +0100

    build: Publish Snapshots bash - docs deployment on github docs repo temporarily disabled

--- scripts/jenkins/publish-snapshots.sh
+++ scripts/jenkins/publish-snapshots.sh
@@ -12,4 +12,4 @@ cd $(dirname ${0})/../..
 
 # Deploy the docs content to the Github repository. We don't want to build the examples
 # package here again because it's already fetched from the Jenkins workspace storage.
-./scripts/jenkins/publish-docs-content.sh
+#./scripts/jenkins/publish-docs-content.sh
