4103fe2a3c7f366c3190cbbb87e35a7247ef7285
Sara <sara.repossi@gnucoop.com>
2024-12-13 13:59:14 +0100

    feat(core/forms): Set the default size limit condition for file input fields if no size limit condition is found

---
+++ projects/core/forms/src/utils/fields-instances/is-file-field-instance.ts
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Copyright (C) Gnucoop soc. coop.
+ *
+ * This file is part of the Advanced JSON forms (ajf).
+ *
+ * Advanced JSON forms (ajf) is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Advanced JSON forms (ajf) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Advanced JSON forms (ajf).
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {AjfFieldType} from '../../interface/fields/field-type';
+import {AjfNodeInstance} from '../../interface/nodes-instances/node-instance';
+import {isFieldInstance} from '../nodes-instances/is-field-instance';
+
+/**
+ * It is true if the nodeInstance is a fieldInstance and
+ * if the node of instance is a File field.
+ */
+export function isFileFieldInstance(nodeInstance: AjfNodeInstance): boolean {
+  return (
+    nodeInstance != null &&
+    isFieldInstance(nodeInstance) &&
+    nodeInstance.node.fieldType === AjfFieldType.File
+  );
+}
