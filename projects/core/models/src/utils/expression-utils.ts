49ba875daf89c2115a6b4fd0bc7efa31e27ec0be
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2026-05-24 12:11:52 +0200

    refactor(core/models): remove unused expression utils

--- projects/core/models/src/utils/expression-utils.ts
+++ projects/core/models/src/utils/expression-utils.ts
@@ -21,7 +21,6 @@
  */
 
 import {AjfContext} from '@ajf/core/common';
-import * as dateFns from 'date-fns';
 import {tokTypes, tokenizer} from 'acorn';
 import {AjfTableCell} from '@ajf/core/table';
 import {AjfValidationFn} from '../interface/validation-function';
@@ -97,56 +96,24 @@ export function getArgumentNames(source: string): Set<string> {
   return identifiers;
 }
 
-export const dateUtils = {
-  addDays: dateFns.addDays,
-  addMonths: dateFns.addMonths,
-  addYears: dateFns.addYears,
-  endOfISOWeek: dateFns.endOfISOWeek,
-  format: dateFns.format,
-  getDay: dateFns.getDay,
-  parse: dateFns.parseISO,
-  startOfMonth: dateFns.startOfMonth,
-  startOfISOWeek: dateFns.startOfISOWeek,
-  isBefore: dateFns.isBefore,
-};
-
 export class AjfExpressionUtils {
   /**
    * It is a key-value dictionary, that mapping all Ajf validation functions.
    */
   static utils: {[name: string]: AjfValidationFn} = {
-    alert: {fn: alert},
     buildAlignedDataset: {fn: buildAlignedDataset},
     buildAlignedFormDataset: {fn: buildAlignedFormDataset},
     buildDataset: {fn: buildDataset},
     buildFormDataset: {fn: buildFormDataset},
     buildWidgetDataset: {fn: buildWidgetDataset},
     buildWidgetDatasetWithDialog: {fn: buildWidgetDatasetWithDialog},
-    calculateAvgProperty: {fn: calculateAvgProperty},
-    calculateAvgPropertyArray: {fn: calculateAvgPropertyArray},
-    calculateTrendByProperties: {fn: calculateTrendByProperties},
-    calculateTrendProperty: {fn: calculateTrendProperty},
-    dateOperations: {fn: dateOperations},
     decimalCount: {fn: decimalCount},
     digitCount: {fn: digitCount},
-    drawThreshold: {fn: drawThreshold},
-    extractArray: {fn: extractArray},
-    extractArraySum: {fn: extractArraySum},
-    extractDates: {fn: extractDates},
-    extractSum: {fn: extractSum},
-    formatDate: {fn: formatDate},
-    getCoordinate: {fn: getCoordinate},
     isInt: {fn: isInt},
-    isoMonth: {fn: isoMonth},
-    lastProperty: {fn: lastProperty},
     notEmpty: {fn: notEmpty},
-    parseDate: {fn: dateUtils.parse},
     plainArray: {fn: plainArray},
     buildPointData: {fn: buildPointData},
     round: {fn: round},
-    scanGroupField: {fn: scanGroupField},
-    sum: {fn: sum},
-    sumLastProperties: {fn: sumLastProperties},
     valueInChoice: {fn: valueInChoice},
     ADD_DAYS: {fn: ADD_DAYS},
     ALL_VALUES_OF: {fn: ALL_VALUES_OF},
@@ -307,6 +274,7 @@ export function digitCount(x: number): number {
   }
   return x.toString().replace(/[^0-9]/g, '').length;
 }
+
 /**
  * It is count the count of decimal digit inside s.
  */
@@ -320,6 +288,7 @@ export function decimalCount(x: string | number): number {
   const parts = x.toString().split('.');
   return parts.length > 1 ? parts[1].length : 0;
 }
+
 /**
  * It is true if x is an integer.
  */
@@ -332,60 +301,19 @@ export function isInt(x: string | number): boolean {
   }
   return false;
 }
+
 /**
  * It is true if x is not empty.
  */
 export function notEmpty(x: any): boolean {
   return !(x == null || x.toString() === '');
 }
+
 /**
  * It is true if array contains x or array is equal to x.
  */
 export function valueInChoice(array: any[], x: any): boolean {
   return (array || []).indexOf(x) > -1 || array === x;
-}
-/**
- * It applies callback for reps times and accumulate the result in acc.
- */
-export function scanGroupField(reps: number, acc: any, callback: any): any {
-  for (let i = 0; i < reps; i++) {
-    acc = callback(acc, i);
-  }
-  return acc;
-}
-/**
- * It returns the sum of the array values.
- */
-export function sum(array: any[]): any {
-  return array.reduce((a, b) => a + b, 0);
-}
-/**
- * It applies add/remove(operation) v (day/month/year)period to dstring and return new format date.
- */
-// TODO check if deprecated instead refacotoring parameter type
-// TODO (dString: string|null, period:'day'|'month'|'year',
-// TODO operation: 'add/remove' = 'add', v:number)
-export function dateOperations(dString: string, period: string, operation: string, v: any): string {
-  const fmt = 'mm/dd/yyyy';
-  let d = typeof dString !== 'undefined' ? dateUtils.parse(dString) : new Date();
-  if (operation == 'remove') {
-    v = -v;
-  }
-  let dateOp;
-  switch (period) {
-    case 'day':
-      dateOp = dateUtils.addDays;
-      break;
-    case 'month':
-      dateOp = dateUtils.addMonths;
-      break;
-    case 'year':
-      dateOp = dateUtils.addYears;
-      break;
-    default:
-      return '';
-  }
-  return dateUtils.format(dateOp(d, v), fmt);
 }
 
 /**
@@ -405,343 +333,6 @@ export function round(n: number | string, digits: number = 0): number {
   n = Number(n);
   const m = Math.pow(10, digits);
   return Math.round(n * m) / m;
-}
-/**
- * It extracts property from source.
- * for every element of source if property and property2 are defined return the sum
- * else if only property is defined return him.
- */
-export function extractArray(source: any[], property: string, property2?: string): any[] {
-  source = (source || []).filter((f: any) => f != null);
-  const res: any[] = [];
-  for (const s of source) {
-    if (s[property] != null && property2 != null && s[property2] != null) {
-      res.push(Number(s[property]) + Number(s[property2]));
-    } else if (s[property] != null) {
-      res.push(s[property]);
-    }
-  }
-  return res;
-}
-/**
- * It returns the sum of all defined properties of each element of source.
- */
-export function extractSum(source: any[], properties: string[]): number {
-  properties = [...(properties || [])];
-
-  let sumVal = 0;
-  for (const prop of properties) {
-    const array = extractArray(source, prop);
-    for (const a of array) {
-      if (!isNaN(Number(a))) {
-        sumVal += Number(a);
-      }
-    }
-  }
-  return sumVal;
-}
-/**
- * It returns a number array that contains the sum of properties value inside the source.
- * extractArraySum([{a: 5}, {b: 1}, {a: 5, b: 1}], ['a', 'b']); =&gt; [6,6]
- */
-export function extractArraySum(source: any[], properties: string[]): any[] {
-  const arrays: any[] = [];
-  properties = [...(properties || [])];
-
-  for (let i = 0; i < properties.length; i++) {
-    const array = extractArray(source, properties[i]);
-    arrays.push(array);
-  }
-
-  const res: any[] = [];
-  if (arrays.length > 0) {
-    for (let weekI = 0; weekI < arrays[0].length; weekI++) {
-      let sumVal = 0;
-      for (let propI = 0; propI < properties.length; propI++) {
-        sumVal = sumVal + Number(arrays[propI][weekI]);
-      }
-      res.push(sumVal);
-    }
-  }
-  return res;
-}
-/**
- * Draw a threshold line on chart related to the property.
- */
-export function drawThreshold(source: any[], property: string, threshold: any[]): any[] {
-  source = (source || []).filter((f: any) => f != null);
-  threshold = threshold || [0];
-  if (!(threshold instanceof Array)) {
-    threshold = [threshold];
-  }
-  const l = source.length;
-  const res: any[] = [];
-  let count = 0;
-  for (let i = 0; i < l; i++) {
-    if (source[i][property] != null) {
-      if (threshold.length > count) {
-        res.push(threshold[count]);
-      } else {
-        res.push(threshold[0]);
-      }
-      count++;
-    }
-  }
-  return res;
-}
-/**
- * Extract the dates of the source object with property != null
- */
-export function extractDates(source: any[], property: string, fmt: string): string[] {
-  source = (source || []).filter((f: any) => f != null);
-  const l = source.length;
-  const res: any = [];
-  let prefix = '';
-  for (let i = 0; i < l; i++) {
-    if (source[i][property] != null) {
-      switch (fmt) {
-        case 'WW':
-        case 'ww':
-          prefix = 'W';
-          res.push(prefix + formatDate(source[i]['date_start'], fmt));
-          break;
-        case 'MM':
-        case 'mm':
-          prefix = 'M';
-          res.push(prefix + isoMonth(source[i]['date_start'], fmt));
-          break;
-        default:
-          prefix = '';
-          res.push(prefix + formatDate(source[i]['date_start'], fmt));
-      }
-    }
-  }
-  return res;
-}
-/**
- * Extract the last property contains in source != null
- */
-export function lastProperty(source: any, property: string): any {
-  source = (source || []).filter((f: any) => f != null);
-  let l = source.length - 1;
-
-  while (l >= 0 && source[l][property] == null) {
-    l--;
-    if (l < 0) {
-      return '';
-    }
-  }
-  return l >= 0 ? source[l][property] : '';
-}
-/**
- * It sum the LAst properties of source.
- */
-export function sumLastProperties(source: any[], properties: string[]): number {
-  source = (source || []).filter((f: any) => f != null);
-  let sumVal = 0;
-  let val = 0;
-  for (let i = 0; i < properties.length; i++) {
-    val = Number(lastProperty(source, properties[i]));
-    if (!isNaN(val)) {
-      sumVal += val;
-    }
-  }
-  return sumVal;
-}
-/**
- * Compute the trend of the property contained on the source.
- */
-export function calculateTrendProperty(source: any[], property: string): string {
-  source = (source || []).filter((f: any) => f != null);
-  let last = source.length - 1;
-  while (source[last][property] == null) {
-    if (last == 0) {
-      break;
-    }
-    last--;
-  }
-  let lastLast = last - 1;
-  if (last == 0) {
-    lastLast = last;
-  } else {
-    while (source[lastLast][property] == null) {
-      if (lastLast == 0) {
-        lastLast = last;
-        break;
-      }
-      lastLast--;
-    }
-  }
-
-  const lastProp = source[last] ? source[last][property] || 0 : 0;
-  const lastLastProp = source[lastLast] ? source[lastLast][property] || 0 : 0;
-
-  if (lastProp == lastLastProp) {
-    return '<p><i class="material-icons" style="color:blue">trending_flat</i></p>';
-  } else if (lastProp > lastLastProp) {
-    return '<p><i class="material-icons" style="color:green">trending_up</i></p>';
-  } else {
-    return '<p><i class="material-icons" style="color:red">trending_down</i></p>';
-  }
-}
-/**
- * Compute the average value of the property contained on the source.
- */
-export function calculateTrendByProperties(source: any[], properties: string[]): string {
-  const arraysum = extractArraySum(source, properties);
-
-  const lastProp = arraysum.length > 0 ? arraysum[arraysum.length - 1] || 0 : 0;
-  const lastLastProp = arraysum.length > 1 ? arraysum[arraysum.length - 2] || 0 : lastProp;
-
-  if (lastProp == lastLastProp) {
-    return '<p><i class="material-icons" style="color:blue">trending_flat</i></p>';
-  } else if (lastProp > lastLastProp) {
-    return '<p><i class="material-icons" style="color:green">trending_up</i></p>';
-  } else {
-    return '<p><i class="material-icons" style="color:red">trending_down</i></p>';
-  }
-}
-/**
- *
- */
-export function calculateAvgProperty(
-  source: any[],
-  property: string,
-  range: number,
-  coefficient: number,
-): number {
-  source = (source || []).filter((f: any) => f != null);
-
-  coefficient = coefficient || 1;
-  range = range || 12;
-
-  let l = source.length;
-  let res = 0;
-  let counter = 0;
-  let noZero = 0;
-
-  if (l < range) {
-    range = l;
-  }
-
-  while (range != 0) {
-    if (source[l - 1][property] != null) {
-      counter++;
-      res += Number(source[l - 1][property]);
-
-      if (source[l - 1][property] > 0) {
-        noZero++;
-      }
-    }
-    l--;
-    range--;
-  }
-
-  if (coefficient == 0) {
-    return noZero;
-  } else {
-    return round((res / counter) * coefficient, 2) || 0;
-  }
-}
-
-export function calculateAvgPropertyArray(
-  source: any[],
-  properties: string[],
-  range: number,
-  coefficient: number,
-): number[] {
-  source = (source || []).filter((f: any) => f != null);
-  const resArr: any[] = [];
-
-  if (properties && properties.length > 0) {
-    let avg = 0;
-
-    coefficient = coefficient || 1;
-    range = range || 12;
-
-    const sourceArr =
-      properties.length > 1
-        ? extractArraySum(source, properties)
-        : extractArray(source, properties[0]);
-
-    let l = sourceArr.length;
-
-    for (let len = l; len > 0; len--) {
-      let res = 0;
-      let counter = 0;
-      let noZero = 0;
-
-      if (len < range) {
-        range = len;
-      }
-
-      for (let r = 1; r <= range; r++) {
-        let val = sourceArr[len - r];
-        if (val != null) {
-          counter++;
-          res += Number(val);
-          if (val > 0) {
-            noZero++;
-          }
-        }
-      }
-
-      if (counter > 0) {
-        if (coefficient == 0) {
-          avg = noZero;
-        } else {
-          avg = (res / counter) * coefficient || 0;
-        }
-        resArr.push(round(avg, 2));
-      }
-    }
-  }
-  return resArr.reverse();
-}
-
-export function alert(source: any[], property: string, threshold: number): string {
-  source = [...(source || [])];
-
-  if (lastProperty(source, property) > threshold) {
-    return '<p><i class="material-icons" style="color:red">warning</i></p>';
-  } else {
-    return '<p></p>';
-  }
-}
-
-export function formatDate(date: Date | string, fmt?: string): string {
-  fmt = fmt || 'mm-DD-yyyy';
-  return dateUtils.format(typeof date === 'string' ? dateUtils.parse(date) : date, fmt);
-}
-
-export function isoMonth(date: Date, fmt?: string): string {
-  fmt = fmt || 'mm';
-  const du = dateUtils;
-  return du.format(du.addDays(du.startOfISOWeek(date), 3), fmt);
-}
-
-export function getCoordinate(source: any, zoom?: number): [number, number, number] {
-  zoom = zoom || 6;
-  if (source == null) {
-    return [51.505, -0.09, zoom];
-  } else {
-    return [source[0], source[1], zoom];
-  }
-}
-/**
- * It creates an one dimensional array of AjfNode.
- * If the node is a containerNode(has the nodes attribute)
- * recursively  concat their nodes.
- */
-function flattenNodes(nodes: any[]): any[] {
-  let flatNodes: any[] = [];
-  nodes.forEach(node => {
-    flatNodes.push(node);
-    if (node != null && (node.nodeType === 3 || node.nodeType === 4)) {
-      flatNodes = flatNodes.concat(flattenNodes(node.nodes));
-    }
-  });
-  return flatNodes;
 }
 
 /**
@@ -1778,6 +1369,22 @@ function extractLabelsFromChoices(schema: any): {[value: string]: string} {
     }
   }
   return labels;
+}
+
+/**
+ * It creates an one dimensional array of AjfNode.
+ * If the node is a containerNode(has the nodes attribute)
+ * recursively  concat their nodes.
+ */
+function flattenNodes(nodes: any[]): any[] {
+  let flatNodes: any[] = [];
+  nodes.forEach(node => {
+    flatNodes.push(node);
+    if (node != null && (node.nodeType === 3 || node.nodeType === 4)) {
+      flatNodes = flatNodes.concat(flattenNodes(node.nodes));
+    }
+  });
+  return flatNodes;
 }
 
 /**
