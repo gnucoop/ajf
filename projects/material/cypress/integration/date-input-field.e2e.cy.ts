867f6b3f03e19d5a8e6a5a7d804d4aaf28a3b223
Roberto Zanotto <roberto.zanotto@gnucoop.com>
2026-07-10 15:14:25 +0200

    fix(material/cypress): fix e2e tests for date input

--- projects/material/cypress/integration/date-input-field.e2e.cy.ts
+++ projects/material/cypress/integration/date-input-field.e2e.cy.ts
@@ -10,10 +10,23 @@ describe('ajf-date-input-field', () => {
     cy.get('mat-datepicker').should('exist');
   });
 
-  it('should prevent selecting dates before minDate and after maxDate', () => {
+  it('should apply the selected date when OK is clicked', () => {
     const today = new Date();
     cy.get('.mat-datepicker-toggle').click();
     cy.get('.mat-calendar-body-cell.mat-calendar-body-active').click();
+    cy.get('[matDatepickerApply]').click();
     cy.get('input').should('have.value', format(today, 'M/d/yyyy'));
+  });
+
+  it('should clear the date and close the datepicker when the clear action is used', () => {
+    cy.get('.mat-datepicker-toggle').click();
+    cy.get('.mat-calendar-body-cell.mat-calendar-body-active').click();
+    cy.get('[matDatepickerApply]').click();
+    cy.get('input').should('not.have.value', '');
+
+    cy.get('.mat-datepicker-toggle').click();
+    cy.contains('button', 'Clear').click();
+    cy.get('mat-datepicker-content').should('not.exist');
+    cy.get('input').should('have.value', '');
   });
 });
